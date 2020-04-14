ca211bc5393ad3174a8ea6d85f123d3848842dce
trejopl <trejo85@live.com.mx>
2020-04-14 09:39:22 -0500

    Template configurado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { dashboardRoutes } from "./dashboard/dashboard.routes";
+import { RegisterComponent } from "./auth/register/register.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "", component: DashboardComponent, children: dashboardRoutes },
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
